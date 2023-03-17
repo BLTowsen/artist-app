1ccfd39d1514b25937cbde33a679d1ece30e70eb
Lawrance Towsen <lawrancetowsen@gmail.com>
2023-03-17 08:02:53 -0400

    fix

--- src/Artist/ArtistProfile.js
+++ src/Artist/ArtistProfile.js
@@ -1,50 +1,40 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import "./ArtistProfile.css"
 import albumArt from "../Assets/album_art.jpg";
 import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
-// add artist variable to the props
+
 const ArtistProfile = () => {
   const { id } = useParams();
-  // create a state variable for the artist
-  const [artist, setArtist] = React.useState({
+  const [artist, setArtist] = useState({
     name: '',
     genres: [],
     popularity: 0,
     mbid: ''
   });
 
-  // get artist name from url params
   useEffect(() => {
-    // get the artist name from the url
     const artistName = id;
-    // fetch the artist info
     fetch(`http://localhost:3001/artists/${artistName}`)
       .then(data => {
-        // read the response
         data.json().then(response => {
-          // set the artist state variable
           setArtist(response);
         });
       });
-  }, []);
+  }, [id]); // add id as a dependency
 
-  return (
-    // get musician name from artist variable
+  // add a conditional check to only render the component once artist state variable has been updated
+  return artist.name ? (
     <div className="artist-profile">
       <h1>{artist.name}</h1>
       <div className="artist-info">
-        {/* list the genres */}
         <div className="genres">
           Genres: {artist.genres.map((genre, index) => <span key={index}>{genre}</span>)}
         </div>
-        {/* list the popularity */}
         <div className="popularity">Popularity: {artist.popularity}</div>
-        {/* list the mbid */}
         <div className="mbid">MBID: {artist.mbid}</div>
       </div>
     </div>
-
-  );
+  ) : null;
 }
 
 export default ArtistProfile;
